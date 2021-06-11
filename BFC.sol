b10d7bea98a0cae5204844b8e161c7d89dde004d
zp <286768756@qq.com>
2021-06-11 19:11:04 +0800

    210611-5+router

--- BFC.sol
+++ BFC.sol
@@ -740,8 +740,8 @@ contract BFC is Context, IERC20, Ownable {
     bool inSwapAndLiquify;
     bool public swapAndLiquifyEnabled = true;
     //max tranfs amount
-    //uint256 public _maxTxAmount = 10*10**6 * 10**6 * 10**9;
-    uint256 public _maxTxAmount = 5*10**8 * 10**6 * 10**9;
+    uint256 public _maxTxAmount = 10*10**6 * 10**6 * 10**9;
+    //uint256 public _maxTxAmount = 5*10**8 * 10**6 * 10**9;
     uint256 private numTokensSellToAddToLiquidity = 500000 * 10**6 * 10**9;
    
     //address    private  autoDestroy = 0x00000000000000000000001;
